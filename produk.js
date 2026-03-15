232c127865d72470be5ef4141a0d4cd4c426cd25
Faiiz <72661180+FaiizOfficial@users.noreply.github.com>
2026-03-16 01:18:30 +0700

    Update produk.js

--- produk.js
+++ produk.js
@@ -1,27 +1,21 @@
 // File: produk.js
 
-// 1. Data bawaan (Cuma dipakai kalau memori browser bener-bener kosong)
 const dataDefault = [
     { id: 1, nama: "Nugget Ayam Fiesta 500g", kategori: "Olahan Ayam", harga: 45000, stok: 24 },
     { id: 2, nama: "Sosis Bakar Champ 1Kg", kategori: "Olahan Sapi", harga: 65000, stok: 5 },
     { id: 3, nama: "Kentang Shoestring Farm Frites 1Kg", kategori: "Cemilan", harga: 35000, stok: 0 }
 ];
 
+let dataLokal = localStorage.getItem('dataStokFitra');
 let daftarProduk = [];
 
-// 2. Cek memori browser
-let dataSimpanan = localStorage.getItem('dataStokFitra');
-
-if (dataSimpanan) {
-    // Kalau browser inget datanya, pakai data dari browser (termasuk yang baru ditambah)
-    daftarProduk = JSON.parse(dataSimpanan);
-} else {
-    // Kalau kosong, pakai data bawaan
+if (dataLokal === null) {
     daftarProduk = dataDefault;
     localStorage.setItem('dataStokFitra', JSON.stringify(daftarProduk));
+} else {
+    daftarProduk = JSON.parse(dataLokal);
 }
 
-// 3. Fungsi untuk nyimpen perubahan ke browser
 function simpanData() {
     localStorage.setItem('dataStokFitra', JSON.stringify(daftarProduk));
 }
