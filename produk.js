d5fd1855ce13959a28c2e1d7c2346a6cc21275c1
Faiiz <72661180+FaiizOfficial@users.noreply.github.com>
2026-03-16 01:04:48 +0700

    Update produk.js

--- produk.js
+++ produk.js
@@ -1,19 +1,24 @@
-// Data awal (akan dipakai kalau web baru pertama kali dibuka)
+// File: produk.js
+
 const dataDefault = [
     { id: 1, nama: "Nugget Ayam Fiesta 500g", kategori: "Olahan Ayam", harga: 45000, stok: 24 },
     { id: 2, nama: "Sosis Bakar Champ 1Kg", kategori: "Olahan Sapi", harga: 65000, stok: 5 },
     { id: 3, nama: "Kentang Shoestring Farm Frites 1Kg", kategori: "Cemilan", harga: 35000, stok: 0 }
 ];
 
-// Ambil data dari localStorage, kalau kosong (null), pakai dataDefault
-let daftarProduk = JSON.parse(localStorage.getItem('dataStokFitra'));
+// Ambil data dari localStorage browser
+let dataLokal = localStorage.getItem('dataStokFitra');
+let daftarProduk = [];
 
-if (!daftarProduk) {
+// Cek apakah udah ada data tersimpan. Kalau belum, pakai dataDefault
+if (dataLokal === null) {
     daftarProduk = dataDefault;
     localStorage.setItem('dataStokFitra', JSON.stringify(daftarProduk));
+} else {
+    daftarProduk = JSON.parse(dataLokal);
 }
 
-// Fungsi andalan buat nyimpen tiap ada perubahan data
+// Fungsi utama buat nyimpen tiap ada nambah/edit/hapus
 function simpanData() {
     localStorage.setItem('dataStokFitra', JSON.stringify(daftarProduk));
 }
