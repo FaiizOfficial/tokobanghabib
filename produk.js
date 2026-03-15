d851e7acf741ed56d2e87b65334431b8046741cb
Faiiz <72661180+FaiizOfficial@users.noreply.github.com>
2026-03-16 00:50:49 +0700

    Create produk.js

---
+++ produk.js
@@ -0,0 +1,27 @@
+// File: produk.js
+// Ini adalah pusat data barang. Edit, tambah, atau hapus barang dari sini agar semua menu otomatis berubah.
+
+const daftarProduk = [
+    { 
+        id: 1, 
+        nama: "Nugget Ayam Fiesta 500g", 
+        kategori: "Olahan Ayam", 
+        harga: 45000, 
+        stok: 24 
+    },
+    { 
+        id: 2, 
+        nama: "Sosis Bakar Champ 1Kg", 
+        kategori: "Olahan Sapi", 
+        harga: 65000, 
+        stok: 5 
+    },
+    { 
+        id: 3, 
+        nama: "Kentang Shoestring Farm Frites 1Kg", 
+        kategori: "Cemilan", 
+        harga: 35000, 
+        stok: 0 
+    }
+    // Kalau mau tambah barang, copy paste format di atas ke baris bawahnya
+];
